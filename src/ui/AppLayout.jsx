ecd6c84c3fef60be1b612808350fad03fed06ddb
unknown <zignale92@gmail.com>
2025-05-10 14:47:10 +0200

    loader

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,22 @@
+/* eslint-disable */
 import Header from "./Header";
+import Loader from "./Loader";
 import CartOverview from './../features/cart/CartOverview';
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+
 export default function AppLayout() 
 {
+
+    const navigation = useNavigate();
+
+    const isLoading = navigation.state === 'loading';
+
+    
     return (
-        <div>
+        <div className="layout">
+
+            {isLoading && <Loader />}
+
             <Header />
 
             <main>
---
+++ src/ui/Loader.jsx
@@ -0,0 +1,9 @@
+import react from 'react'
+
+export default function Loader() {
+  return (
+    <div className='loader'>
+        Loader
+    </div>
+  )
+}
